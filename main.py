2af84c1c6d7e9b503f28f97ed298f1cc05dbf640
Vicky <149935112+Vicky-258@users.noreply.github.com>
2025-05-03 17:25:03 +0530

    Revert "Started refactoring"
    This reverts commit 9f508ca36db5c50c102540628180a44e60bd675e.

--- main.py
+++
@@ -1,8 +0,0 @@
-from services.DB import create_tables
-
-def main():
-
-    create_tables()
-
-if __name__ == "__main__":
-    main()
--- models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .item import Items
-from .category import Categories
-from .supplier import Suppliers
-from .transaction import ItemTransactions, TransactionType
